9526d5b01f9183bac8042be9beb286b60d9c815c
karthickumar <karthickumar_g@hotmail.com>
2021-09-03 21:03:14 +0530

    Redux Thunk Changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { todoReducer } from "./reducer/addTodo.reducer";
+import { userReducer } from "./reducer/user.reducer";
 import { logger } from "redux-logger";
+import thunk from "redux-thunk";
 
-export const todoStore = createStore(todoReducer, applyMiddleware(logger));
+const rootReducer = combineReducers({ todoReducer, userReducer });
+export const todoStore = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger)
+);
